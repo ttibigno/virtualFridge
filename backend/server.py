137d13bbe333b24d32eec2608f7d7bf253ee6684
ttibigno <61109682+ttibigno@users.noreply.github.com>
2026-08-19 15:00:25 +0200

    added sqlalchemy and psycopg

--- backend/server.py
+++ backend/server.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI, APIRouter
 from routers.fridge import fridgeRouter as fr
+from sqlalchemy import create_engine
+from sqlalchemy.orm import Session
+
+#https://docs.sqlalchemy.org/en/20/core/engines.html#sqlalchemy.create_engine
+engine = create_engine("postgresql+psycopg://postgresql:password@localhost/postgresql")
 
 app = FastAPI()
 
 app.include_router(fr)
+
+#with Session(engine) as session:
+#    session.commit()
